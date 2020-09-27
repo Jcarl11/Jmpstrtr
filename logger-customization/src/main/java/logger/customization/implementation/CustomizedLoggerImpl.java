7461574835d5d22e57e6db403c6579a5bb908b92
Joey <joeycarlofrancisco@gmail.com>
2020-09-27 15:37:51 +0800

    Renamed CustomizedLogger to CustomizedLoggerImpl and modified implementation

--- logger-customization/src/main/java/logger/customization/implementation/CustomizedLoggerImpl.java
+++ logger-customization/src/main/java/logger/customization/implementation/CustomizedLoggerImpl.java
@@ -3,88 +3,88 @@ package logger.customization.implementation;
 import ch.qos.logback.classic.Logger;
 import logger.customization.repository.interfaces.CustomizationLogger;
 
-public class CustomizedLogger implements CustomizationLogger {
-	
-	//TODO: Implement dependency injection here..
-	private Logger logger;
-	
-	public CustomizedLogger(Logger logger) {
+public class CustomizedLoggerImpl implements CustomizationLogger {
+
+	// TODO: Implement dependency injection here..
+	private BaseLoggerBuilder<?> logger;
+
+	public CustomizedLoggerImpl( BaseLoggerBuilder<?> logger ) {
 		this.logger = logger;
 	}
 
 	@Override
 	public void trace(String msg) {
-		logger.trace(msg);
+		logger.logger().trace(msg);
 	}
 
 	@Override
 	public void trace(String format, Object ... arguments) {
-		logger.trace(format, arguments);
+		logger.logger().trace(format, arguments);
 	}
 
 	@Override
 	public void trace(String msg, Throwable t) {
-		logger.trace(msg, t);
+		logger.logger().trace(msg, t);
 	}
 
 	@Override
 	public void debug(String msg) {
-		logger.debug(msg);
+		logger.logger().debug(msg);
 	}
 
 	@Override
 	public void debug(String format, Object ... arguments) {
-		logger.debug(format, arguments);
+		logger.logger().debug(format, arguments);
 	}
 
 	@Override
 	public void debug(String msg, Throwable t) {
-		logger.debug(msg, t);
+		logger.logger().debug(msg, t);
 	}
 
 	@Override
 	public void info(String msg) {
-		logger.info(msg);
+		logger.logger().info(msg);
 	}
 
 	@Override
 	public void info(String format, Object ... arguments) {
-		logger.info(format, arguments);
+		logger.logger().info(format, arguments);
 	}
 
 	@Override
 	public void info(String msg, Throwable t) {
-		logger.info(msg, t);
+		logger.logger().info(msg, t);
 	}
 
 	@Override
 	public void warn(String msg) {
-		logger.warn(msg);
+		logger.logger().warn(msg);
 	}
 
 	@Override
 	public void warn(String format, Object ... arguments) {
-		logger.warn(format, arguments);
+		logger.logger().warn(format, arguments);
 	}
 
 	@Override
 	public void warn(String msg, Throwable t) {
-		logger.warn(msg, t);
+		logger.logger().warn(msg, t);
 	}
 
 	@Override
 	public void error(String msg) {
-		logger.error(msg);
+		logger.logger().error(msg);
 	}
 
 	@Override
 	public void error(String format, Object ... arguments) {
-		logger.error(format, arguments);
+		logger.logger().error(format, arguments);
 	}
 
 	@Override
 	public void error(String msg, Throwable t) {
-		logger.error(msg, t);
+		logger.logger().error(msg, t);
 	}
 
 }
