841b9c1e1bdc02c076b0bb880d23625954f3ef91
Joey <joeycarlofrancisco@gmail.com>
2020-10-12 21:46:58 +0800

    Refactored to use assertEquals instead

--- logger-customization/src/test/java/logger/customization/custom/filters/InformationalFilterTest.java
+++ logger-customization/src/test/java/logger/customization/custom/filters/InformationalFilterTest.java
@@ -1,6 +1,6 @@
 package logger.customization.custom.filters;
 
-import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -27,48 +27,42 @@ public class InformationalFilterTest {
 	public void INFO_Reply_test() {
 		when(iLoggingEventStub.getLevel()).thenReturn(Level.INFO);
 		FilterReply fileFilterReply = informationalFilter.decide(iLoggingEventStub);
-		assertArrayEquals("Filter with reply of INFO failed to accept", new FilterReply[] {FilterReply.ACCEPT},
-			new FilterReply[] {fileFilterReply});
+		assertEquals("Filter with reply of INFO failed to accept", FilterReply.ACCEPT, fileFilterReply);
 	}
 
 	@Test
 	public void DEBUG_Reply_test() {
 		when(iLoggingEventStub.getLevel()).thenReturn(Level.DEBUG);
 		FilterReply fileFilterReply = informationalFilter.decide(iLoggingEventStub);
-		assertArrayEquals("Filter with reply of DEBUG failed to accept", new FilterReply[] {FilterReply.ACCEPT},
-			new FilterReply[] {fileFilterReply});
+		assertEquals("Filter with reply of DEBUG failed to accept", FilterReply.ACCEPT, fileFilterReply);
 	}
 
 	@Test
 	public void WARN_Reply_test() {
 		when(iLoggingEventStub.getLevel()).thenReturn(Level.WARN);
 		FilterReply fileFilterReply = informationalFilter.decide(iLoggingEventStub);
-		assertArrayEquals("Filter reply other than INFO and DEBUG was accepted", new FilterReply[] {FilterReply.DENY},
-			new FilterReply[] {fileFilterReply});
+		assertEquals("Filter reply other than INFO and DEBUG was accepted", FilterReply.DENY, fileFilterReply);
 	}
 
 	@Test
 	public void ERROR_Reply_test() {
 		when(iLoggingEventStub.getLevel()).thenReturn(Level.ERROR);
 		FilterReply fileFilterReply = informationalFilter.decide(iLoggingEventStub);
-		assertArrayEquals("Filter reply other than INFO and DEBUG was accepted", new FilterReply[] {FilterReply.DENY},
-			new FilterReply[] {fileFilterReply});
+		assertEquals("Filter reply other than INFO and DEBUG was accepted", FilterReply.DENY, fileFilterReply);
 	}
 
 	@Test
 	public void ALL_Reply_test() {
 		when(iLoggingEventStub.getLevel()).thenReturn(Level.ALL);
 		FilterReply fileFilterReply = informationalFilter.decide(iLoggingEventStub);
-		assertArrayEquals("Filter reply other than INFO and DEBUG was accepted", new FilterReply[] {FilterReply.DENY},
-			new FilterReply[] {fileFilterReply});
+		assertEquals("Filter reply other than INFO and DEBUG was accepted", FilterReply.DENY, fileFilterReply);
 	}
 
 	@Test
 	public void OFF_Reply_test() {
 		when(iLoggingEventStub.getLevel()).thenReturn(Level.OFF);
 		FilterReply fileFilterReply = informationalFilter.decide(iLoggingEventStub);
-		assertArrayEquals("Filter reply other than INFO and DEBUG was accepted", new FilterReply[] {FilterReply.DENY},
-			new FilterReply[] {fileFilterReply});
+		assertEquals("Filter reply other than INFO and DEBUG was accepted", FilterReply.DENY, fileFilterReply);
 	}
 
 }
